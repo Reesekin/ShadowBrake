3af6b3921162fabd1e8f42cd1e6a15507e23cbb3
Reesekin <terret343@gmail.com>
2021-12-17 20:44:01 -0700

    Initial commit

---
+++ ShadowCompressor/CLIhandler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ShadowCompressor
+{
+    public class CLIhandler
+    {
+        private Process p;
+        private ProcessStartInfo startInfo;
+        public CLIhandler(String dir)
+        {
+            this.p = new Process();
+            this.startInfo = new ProcessStartInfo();
+            startInfo.FileName = "cmd.exe";
+            startInfo.RedirectStandardInput = true;
+            startInfo.RedirectStandardOutput = false;
+            startInfo.UseShellExecute = false;
+            startInfo.WorkingDirectory = dir; //Directory where HandBrakeCLI.exe is
+            p.StartInfo = startInfo;
+            p.Start();
+        }
+
+        public void exec(String command)
+        {
+            p.StandardInput.WriteLine(command);
+            p.StandardInput.Flush();
+        }        
+    }
+}
